d8b9fe90c6f9c31842111a17c98dc54e2ec0c8b2
dzubi77 <mat.dzubak@gmail.com>
2024-11-21 07:31:19 +0100

    renamed folder for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <h1>Home page</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function Login() {
+    return (
+        <>
+            <Navbar/>
+            <h1>Login page</h1>
+        </>
+    );
+}
+
+export default Login;
